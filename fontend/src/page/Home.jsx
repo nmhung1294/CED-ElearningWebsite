2cbea8c7845377c7bbe59d32f16b3c47a8690999
pj_mam <duyphuocpy2018@gmail.com>
2024-06-27 21:10:48 +0700

    update home page
    complete-->> hero-section
    process-->> about-us

--- fontend/src/page/Home.jsx
+++ fontend/src/page/Home.jsx
@@ -1,11 +1,15 @@
 import React, { Fragment } from 'react';
 
 import Header from "../components/Header/Header";
+import HeroSection from "../components/Hero-section/HeroSection"
+import CompanySection from '../components/Company-section/Company';
 
 const Home = () => {
   return (
    <Fragment>
     <Header/>
+    <HeroSection/>
+    <CompanySection/>
    </Fragment>
   )
 }
